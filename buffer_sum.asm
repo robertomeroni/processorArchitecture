25c64e2093a3814798c977890b8c3c616a1c5d8a
Tim Hennes Wichelmann <tim.wichelmann98@gmail.com>
2024-01-24 23:30:25 +0100

    init array in buffer sum

--- buffer_sum.asm
+++ buffer_sum.asm
@@ -19,10 +19,8 @@ _start:
 	slli	a5,a5,2
 	addi	a4,s0,-16
 	add	a5,a4,a5
-	lw	a5,-520(a5)
-	lw	a4,-20(s0)
-	add	a5,a4,a5
-	sw	a5,-20(s0)
+	li	a4,1
+	sw	a4,-520(a5)
 	lw	a5,-24(s0)
 	addi	a5,a5,1
 	sw	a5,-24(s0)
@@ -30,6 +28,24 @@ _start:
 	lw	a4,-24(s0)
 	li	a5,127
 	ble	a4,a5,.L3
+	sw	zero,-24(s0)
+	j	.L4
+.L5:
+	lw	a5,-24(s0)
+	slli	a5,a5,2
+	addi	a4,s0,-16
+	add	a5,a4,a5
+	lw	a5,-520(a5)
+	lw	a4,-20(s0)
+	add	a5,a4,a5
+	sw	a5,-20(s0)
+	lw	a5,-24(s0)
+	addi	a5,a5,1
+	sw	a5,-24(s0)
+.L4:
+	lw	a4,-24(s0)
+	li	a5,127
+	ble	a4,a5,.L5
 	nop
 	nop
 	lw	s0,540(sp)
--- performanceTest.c
+++ performanceTest.c
@@ -1,10 +1,12 @@
-/* Type your code here, or load an example. */
-/* void _start() */
-/* { */
-/*      int a[128], sum = 0; */
-/*      int i; */
-/*      for (i=0; i<128; i++) { sum += a[i]; } */
-/* } */
+// Type your code here, or load an example.
+
+void _start()
+{
+     int a[128], sum = 0;
+     int i;
+     for (i=0; i<128; i++) { a[i] = 1; }
+     for (i=0; i<128; i++) { sum += a[i]; }
+}
 
 /* void _start() */
 /* { */
@@ -14,16 +16,16 @@
 /*      for (i=0; i<128; i++) { b[i] = a[i]; } */
 /* } */
 
-void _start()
-{
-     int a[128][128], b[128][128], c[128][128];
-     int i, j, k;
-     for (i=0; i<128; i++) {
-	  for(j =0;j<128;j++) {
-	       c[i][j] = 0;
-	       for(k = 0; k <128; k++ ) {
-		    c[i][j] = c[i][j] + a[i][k] * b[k][j];
-	       }
-	  }
-     }
-}
+/* void _start() */
+/* { */
+/*      int a[128][128], b[128][128], c[128][128]; */
+/*      int i, j, k; */
+/*      for (i=0; i<128; i++) { */
+/* 	  for(j =0;j<128;j++) { */
+/* 	       c[i][j] = 0; */
+/* 	       for(k = 0; k <128; k++ ) { */
+/* 		    c[i][j] = c[i][j] + a[i][k] * b[k][j]; */
+/* 	       } */
+/* 	  } */
+/*      } */
+/* } */
